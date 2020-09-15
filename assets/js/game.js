8a80304cd0b9e81605fc3c5a6b8336d093792cce
sas1300 <sascode13@gmail.com>
2020-09-15 15:25:25 -0500

    fixed execution order problem

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,42 +12,42 @@ var enemyAttack = 12;
 console.log(enemyName, enemyAttack, enemyHealth);
 
 var fight = function() {
-    //alert users that they are starting the round
-    window.alert("Welcome to Robot Gladiators");
+        //alert users that they are starting the round
+        window.alert("Welcome to Robot Gladiators");
 
-//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-enemyHealth = enemyHealth - playerAttack; 
-// Log a resulting message to the console so we know that it worked.
+        //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+            enemyHealth = enemyHealth - playerAttack; 
+        // Log a resulting message to the console so we know that it worked.
 
-  console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining ");
-// check enemy's health
+            console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining ");
+        // check enemy's health
 
 
-if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died ");
-}
+            if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died ");
+        }
 
-else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left ");
-}
+            else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left ");
+        }
 
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  playerHealth = playerHealth - enemyAttack;
+        // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+            playerHealth = playerHealth - enemyAttack;
 
-  // Log a resulting message to the console so we know that it worked.
-  console.log(enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining");
+        // Log a resulting message to the console so we know that it worked.
+            console.log(enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining");
 
-}
 
-// check player's health
+        // check player's health
 
-if (playerHealth <= 0) {
-    window.alert(playerName + " has died ");
-}
+            if (playerHealth <= 0) {
+            window.alert(playerName + " has died ");
+         }
 
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left ");
-}
+            else {
+            window.alert(playerName + " still has " + playerHealth + " health left ");
+        }
+    }
 
 
 // execute function
