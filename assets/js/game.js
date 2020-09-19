12ec9bacff4ee5afb8df852e4657ee2f645aba34
sas1300 <sascode13@gmail.com>
2020-09-19 14:42:29 -0500

    functioning fight loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,14 +23,14 @@ for (var i = 0; i < enemyNames.length; i++) {
     console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + " index ");
+}
+var fight = function (enemyName) {
+    //fight function statements
 
-    var fight = function (enemyName) {
-        //fight function statements
 
+    // Log multiple values
+   
 
-        // Log multiple values
-        console.log(enemyNames, enemyAttack, enemyHealth);
-    }
 
 
     //alert users that they are starting the round
@@ -44,19 +44,19 @@ for (var i = 0; i < enemyNames.length; i++) {
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
-        console.log(playerName + " attacked " + enemyNames + " . " + enemyNames + " now has " + enemyHealth + " health remaining.");
+        console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining.");
 
         // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyNames + " has died!");
+            window.alert(enemyName + " has died!");
         }
         else {
-            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
-        console.log(enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
         // check player's health
         if (playerHealth <= 0) {
@@ -68,6 +68,7 @@ for (var i = 0; i < enemyNames.length; i++) {
         }
 
     }
+
     // if player chooses to skip
     else if (promptFight === "skip" || promptFight === "SKIP") {
 
@@ -95,12 +96,13 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // execute function
 
-    for (var i = 0; i < enemyNames.length; i++) {
-        fight(enemyNames[i]);
-    }
 
 }
 
+for (var i = 0; i < enemyNames.length; i++) {
+    
+    fight(enemyNames[i]);
+}
 
 
 // Ask learning assistant about the enemyName that was removed but not replaced with anything
